670a61e3d8a5dd9b59be34c809f49268f89fb900
Karissa <ka.lange266@gmail.com>
2023-03-09 11:56:41 -0500

    add drawing, paint, jewelry, yarn sections and photos

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-let logo = document.getElementById('artByMeLogo');
-let navbar = document.getElementById('mainNavbar');
-let closeButton = document.getElementById('closeNavbar')
+let drawingTab = document.getElementById('drawingHeading');
+let jewelryTab = document.getElementById('jewelryHeading');
+let paintTab = document.getElementById('paintHeading');
+let yarnTab = document.getElementById('yarnHeading');
 
-logo.addEventListener('click', showNavbar);
-closeButton.addEventListener('click', hideNavbar);
-
-
-function showNavbar(){
-    navbar.classList.remove('nav');
-    navbar.classList.add('navOpen');
-}
-function hideNavbar(){
-    navbar.classList.add('nav');
-    navbar.classList.remove('navOpen');
-}
